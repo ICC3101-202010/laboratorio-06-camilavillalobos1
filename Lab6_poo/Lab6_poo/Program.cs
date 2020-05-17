368728bcd067c710778a77cda7c63df2a6f68d60
camilavillalobos1 <civillalobos@miuandes.cl>
2020-05-17 11:03:46 -0400

    First commit

---
+++ Lab6_poo/Lab6_poo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lab6_poo
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
